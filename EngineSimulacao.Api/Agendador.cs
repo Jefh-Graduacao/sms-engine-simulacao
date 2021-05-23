ddfc9aad8fa469739832b5e7026db22e6f1c2b92
André Hoffmann <andre8hoffmann@gmail.com>
2021-05-23 19:23:33 -0300

    Adição pseucódigo Agendador.simulateOneStep()

--- EngineSimulacao.Api/Agendador.cs
+++ EngineSimulacao.Api/Agendador.cs
@@ -2,7 +2,16 @@ namespace EngineSimulacao.Api
 {
     public class Agendador
     {
+        //public Motor motor (adicionado pela lib externa)
         //public List<Evento> FEL (Lista de eventos lançados)
         public int tempo { get; } //em segundos
+    
+        /*
+        simulateOneStep(){
+            chama o motor com o próximo evento do FEL
+                (ou todos os eventos que devem ser executados no 'tempo' atual)
+        }
+        */
+
     }
 }
