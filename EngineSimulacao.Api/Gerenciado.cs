f486e1c6cdc5b9dbc11eebdfa6234f4664960f6c
André Hoffmann <andre8hoffmann@gmail.com>
2021-06-08 21:51:38 -0300

    Documentação classe 'Gerenciado'

--- EngineSimulacao.Api/Gerenciado.cs
+++ EngineSimulacao.Api/Gerenciado.cs
@@ -3,6 +3,10 @@ using System.Reflection;
 
 namespace EngineSimulacao.Api
 {
+    /// <summary>
+    /// Gerencia o Id das instâncias, e o controle de seu 
+    /// histórico através dos históricos relevantes
+    /// </summary>
     public abstract class Gerenciado
     {
         private static int _contadorId;
@@ -13,7 +17,10 @@ namespace EngineSimulacao.Api
         {
             this.Id = _gerarId();
         }
-
+        /// <summary>
+        /// Adiciona info da da criação da instância no histórico 
+        /// de sua classe, e de todas as classes que herda
+        /// </summary>
         protected void _nascerEmTodosOsNiveis(){
             Type tipoAtual = this.GetType();
 
@@ -22,6 +29,10 @@ namespace EngineSimulacao.Api
                 tipoAtual = tipoAtual.BaseType;
             }
         }
+        /// <summary>
+        /// Adiciona info da destruição da instância no histórico 
+        /// de sua classe, e de todas as classes que herda
+        /// </summary>
         protected void _morrerEmTodosOsNiveis(){
             Type tipoAtual = this.GetType();
 
@@ -30,6 +41,12 @@ namespace EngineSimulacao.Api
                 tipoAtual = tipoAtual.BaseType;
             }
         }
+        /// <summary>
+        /// Realiza ade maneira controlada uma chamada do formato:
+        ///
+        ///     Gerenciador<tipo>.nomeMetodo(this);
+        ///
+        /// </summary>
         private void chamarGerenciadorDoTipo(Type tipo, string nomeMetodo){
             var gerenciadorDoTipo = typeof(Gerenciador<>).MakeGenericType(tipo);
             MethodInfo metodo = gerenciadorDoTipo.GetMethod(nomeMetodo);
